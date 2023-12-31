58d81a349e62a433ad611160df5ab0bd3656586f
rokhayatoure2000@gmail.com <rokhayatoure2000gmail.com>
2023-12-31 18:26:28 +0100

    Crud formation

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Formation;
 use App\Repository\FormationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
@@ -18,53 +22,53 @@ class FormationController extends AbstractController
         $jsonformationList = $serializer->serialize($formationList, 'json');
         return new JsonResponse($jsonformationList, Response::HTTP_OK, [], true);
     }
-    //sup formation
-//     #[Route('/api/formation/{id}', name: 'deleteFormation', methods: ['DELETE'])]
-//     public function deleteFormation(Formation $formation, EntityManagerInterface $em): JsonResponse 
-//     {
-//         $em->remove($formation,);
-//         $em->flush();
+    // sup formation
+    #[Route('/api/formation/{id}', name: 'deleteFormation', methods: ['DELETE'])]
+    public function deleteFormation(Formation $formation, EntityManagerInterface $em): JsonResponse 
+    {
+        $em->remove($formation,);
+        $em->flush();
 
-//         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
-//     }
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
     
 //     //ajout formation
     
-//     #[Route('/formationcreat', name: "createFormation", methods: ['POST'])]
-//     public function createFormation(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
-//     {
-//         try {
-//             $formation = $serializer->deserialize($request->getContent(), Formation::class, 'json');
-//             // Enregistrement
-//             $em->persist($formation);
-//             // Confirmer
-//             $em->flush();
+    #[Route('/formationcreat', name: "createFormation", methods: ['POST'])]
+    public function createFormation(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    {
+        try {
+            $formation = $serializer->deserialize($request->getContent(), Formation::class, 'json');
+            // Enregistrement
+            $em->persist($formation);
+            // Confirmer
+            $em->flush();
     
-//             $jsonFormation = $serializer->serialize($formation, 'json', ['groups' => 'getFormations']);
+            $jsonFormation = $serializer->serialize($formation, 'json', ['groups' => 'getFormations']);
     
-//             return new JsonResponse($jsonFormation, Response::HTTP_CREATED);
-//         } catch (\Exception $e) {
-//             // Gérer d'autres types d'exceptions si nécessaire
-//             $error = ['error' => $e->getMessage()];
-//             return new JsonResponse($error, Response::HTTP_BAD_REQUEST);
-//         }
-//     }
-//     //modifi formation
+            return new JsonResponse($jsonFormation, Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            // Gérer d'autres types d'exceptions si nécessaire
+            $error = ['error' => $e->getMessage()];
+            return new JsonResponse($error, Response::HTTP_BAD_REQUEST);
+        }
+    }
+    //modifi formation
 
-//    #[Route('/api/formation/{id}', name:"updateFormation", methods:['PUT'])]
+   #[Route('/api/formation/{id}', name:"updateFormation", methods:['PUT'])]
 
-//     public function updateFormation(Request $request, SerializerInterface $serializer, Formation $currentFormation, EntityManagerInterface $em, ): JsonResponse 
-//     {
-//         $updatedFormation = $serializer->deserialize($request->getContent(), 
-//                 Formation::class, 
-//                 'json', 
-//                 [AbstractNormalizer::OBJECT_TO_POPULATE => $currentFormation]);
-//         // $content = $request->toArray();
-//         // $idAuthor = $content['idAuthor'] ?? -1;
-//         // $updatedFormation->setAuthor($authorRepository->find($idAuthor));
+    public function updateFormation(Request $request, SerializerInterface $serializer, Formation $currentFormation, EntityManagerInterface $em, ): JsonResponse 
+    {
+        $updatedFormation = $serializer->deserialize($request->getContent(), 
+                Formation::class, 
+                'json', 
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $currentFormation]);
+        // $content = $request->toArray();
+        // $idAuthor = $content['idAuthor'] ?? -1;
+        // $updatedFormation->setAuthor($authorRepository->find($idAuthor));
         
-//         $em->persist($updatedFormation);
-//         $em->flush();
-//         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
-// }
+        $em->persist($updatedFormation);
+        $em->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+}
 }
